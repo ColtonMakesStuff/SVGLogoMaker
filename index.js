20a82b430b26465bc4f2bcb4ab257ab0ca87c46a
Colton Firestone <colton.firestone@gmail.com>
2023-08-23 17:12:11 -0700

    fixed file structure and started tests

---
+++ index.js
@@ -0,0 +1,5 @@
+const Prompt = require('./lib/prompt.js');
+
+const promptUser = new Prompt();
+
+promptUser.run();
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,35 +1,30 @@
 const fs = require('fs');
 const Shape = require('../lib/shapes.js');
 
-class Circle extends Shape {
-  constructor(shapeColor, text, textColor) {
-    const shape = `<circle cx="150" cy="100" r="100" />`;
-    super(shape, shapeColor, text, textColor);
-  }
-}
+
+// circle.test.js
+const Circle = require('../lib/shapes.js');
 
 describe('Circle', () => {
   describe("renderLogo", () => {
-    it("should take a user input and render a circle svg file", () => {
+    it("should take a user input and render a circle svg file", async () => {
       // Arrange
       const shapeColor = 'red';
       const text = 'WOO';
       const textColor = 'blue';
+      const outputPath = 'shape.svg';
 
       // Act
       const circleInstance = new Circle(shapeColor, text, textColor);
-      circleInstance.renderLogo();
+      await circleInstance.renderLogo(outputPath);
 
       // Assert
       // Check if the SVG file was saved
-      expect(fs.writeFile).toHaveBeenCalledWith('shape.svg', circleInstance.svgContent, expect.any(Function));
+      expect(fs.writeFile).toHaveBeenCalledWith(outputPath, circleInstance.svgContent, expect.any(Function));
 
       // Check if the console.log messages were called as expected
-      expect(console.log).toHaveBeenCalledWith(`Rendering a 300x200 svg logo with shape:<circle cx="150" cy="100" r="100" />, letters:WOO and colors red for the shape and blue for the letters`);
+      expect(console.log).toHaveBeenCalledWith(`Rendering a 300x200 svg logo with shape:<circle cx="150" cy="100" r="100" />, letters:${text} and colors ${shapeColor} for the shape and ${textColor} for the letters`);
       expect(console.log).toHaveBeenCalledWith('SVG file has been saved.');
-
-      // Clean up
-      fs.unlinkSync('shape.svg');
     });
   });
 });
